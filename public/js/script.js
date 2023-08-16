ffe1dabaaea03282599b566b65bc7fb2509328d7
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-08-17 02:12:58 +0700

    highlight

--- public/js/script.js
+++ public/js/script.js
@@ -57,12 +57,32 @@ const linkedin = document.getElementById("linkedin");
 hover(linkedin);
 
 function isVisible(scrollPosition, element1, element2) {
-  return scrollPosition >= element1.offsetTop && scrollPosition < element2.offsetTop;
+  return scrollPosition > element1.offsetTop && scrollPosition < element2.offsetTop;
+}
+
+function highlight(topElement, target) {
+  const rect = topElement.getBoundingClientRect();
+  if (rect.bottom < 200) {
+    target.classList.add("highlighted");
+  } else {
+    topElement.classList.add("highlighted");
+  }
+  return rect.bottom < 200;
 }
 
 function isVisibleFade(element) {
   const rect = element.getBoundingClientRect();
   return rect.bottom < 400;
+}
+
+function show(element) {
+  element.classList.remove("hidden");
+  element.classList.add("visible");
+}
+
+function hide(element) {
+  element.classList.add("hidden");
+  element.classList.remove("visible");
 }
 
 function fadeIn(element) {
@@ -75,12 +95,27 @@ function fadeOut(element) {
   element.classList.remove("fade-element");
 }
 
+function addDetailClick(target) {
+  detail.addEventListener("click", () => {
+    show(target);
+  });
+}
+
 const nav = document.getElementById("nav-sidebar");
 const sidebar = document.getElementById("sidebar-image");
 const home = document.getElementById("home");
 const about = document.getElementById("about");
 const contact = document.getElementById("contact");
 const detail = document.getElementById("detail");
+
+for (let i = 1; i <= 9; i++) {
+  const variableName = `section${i}`;
+  window[variableName] = document.getElementById(`port${i}`);
+  window[variableName].style.paddingTop = "8px";
+  window[variableName].style.paddingLeft = "8px";
+  window[variableName].style.paddingRight = "8px";
+  window[variableName].style.paddingBottom = "8px";
+}
 
 function handleScroll() {
   const scrollPosition = window.scrollY;
@@ -90,44 +125,45 @@ function handleScroll() {
     const variableName = `section${i}`;
     window[variableName] = document.getElementById(`port${i}`);
     window[variableName].classList.remove("highlighted");
+    const detailDiv = `detailport${i}`;
+    window[detailDiv] = document.getElementById(`detailport${i}`);
   }
 
   if (isVisible(scrollPosition, home, section1)) {
     bgImg = "me";
+    highlight(home, section1);
+    fadeOut(about);
+    fadeOut(contact);
   } else if (isVisible(scrollPosition, section1, section2)) {
     bgImg = "yuubaca";
-    section1.classList.add("highlighted");
-    // detail.addEventListener("click", () => {
-    //   nav.style.width = "0%";
-    //   console.log("clicked");
-    // });
+    highlight(section1, section2);
+    fadeOut(about);
+    fadeOut(contact);
   } else if (isVisible(scrollPosition, section2, section3)) {
     bgImg = "3food";
-    section2.classList.add("highlighted");
+    highlight(section2, section3);
   } else if (isVisible(scrollPosition, section3, section4)) {
     bgImg = "speakuy";
-    section3.classList.add("highlighted");
+    highlight(section3, section4);
   } else if (isVisible(scrollPosition, section4, section5)) {
     bgImg = "storyapp";
-    section4.classList.add("highlighted");
+    highlight(section4, section5);
   } else if (isVisible(scrollPosition, section5, section6)) {
     bgImg = "githubuser";
-    section5.classList.add("highlighted");
+    highlight(section5, section6);
   } else if (isVisible(scrollPosition, section6, section7)) {
     bgImg = "telucanteen";
-    section6.classList.add("highlighted");
+    highlight(section6, section7);
   } else if (isVisible(scrollPosition, section7, section8)) {
     bgImg = "hitungbmi";
-    section7.classList.add("highlighted");
+    highlight(section7, section8);
   } else if (isVisible(scrollPosition, section8, section9)) {
     bgImg = "mobpro2";
-    section8.classList.add("highlighted");
+    highlight(section8, section9);
     fadeOut(about);
     fadeOut(contact);
   } else if (isVisible(scrollPosition, section9, about)) {
     bgImg = "gasdect";
-    section9.classList.add("highlighted");
-
     const rect = section9.getBoundingClientRect();
     if (rect.bottom < 400) {
       fadeIn(about);
@@ -137,12 +173,13 @@ function handleScroll() {
     }
   } else {
     bgImg = "me";
+    fadeIn(about);
+    fadeIn(contact);
     for (let i = 1; i <= 9; i++) {
       const variableName = `section${i}`;
       window[variableName].classList.remove("highlighted");
     }
   }
-
   sidebar.style.backgroundImage = `url("./public/img/${bgImg}.jpg")`;
 }
 
